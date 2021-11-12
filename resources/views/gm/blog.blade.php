b5f3979b01f7c2e713315f45bda6fc0d1dce8ee5
Pedro6Miguel <pedro.miguel.16@hotmail.com>
2021-11-12 22:26:12 +0000

    Update Blog

--- resources/views/gm/blog.blade.php
+++ resources/views/gm/blog.blade.php
@@ -9,53 +9,128 @@
         <h1 id="blog-title">Blog</h1>
     </section>
 
-    <div id="blog-margin">
+    <section id="blog">
+      <p id="publicacoes">Últimas Publicações</p>
     <h4 id="not-title" class="mb-4">Notícias</h4>
+      <!-- Blog-Container -->
+      <div class="blog-container">
 
-    <div class="container-1">
-        <div class="row">
-          <div id="artigos" class="col-lg-4">
-            <img class="blog-img" src="img/artigo2.jpg" alt="">
-            <h3 class="blog-title">Alimentação Vs Treino</h3>
-            <p class="blog-text">Hoje no dia Mundial da Alimentação vamos analisar o impacto da alimentação na atividade física e acredite é bastante!</p>
-            <button type="button" class="btn ">Ver mais</button>
+      <!-- Blog-Box 1 -->
+        <div class="blog-box">
+
+        <!-- Blog-Img -->
+          <div class="blog-img">
+            <img src="img/artigo2.jpg" alt="">
           </div>
-          <div id="artigos" class="col-lg-4">
-            <img class="blog-img" src="img/pexels-leon-ardho-1552242.jpg" alt="">
-            <h3 class="blog-title">Crossfit: Descubra 10 exercícios para um treino completo!</h3>
-            <p class="blog-text">O foco do CrossFit é, sobretudo, realizar conjuntos de exercícios num determinado tempo. Desse modo, constitui-se como um desafio de superação individual ou coletiva.</p>
-            <button type="button" class="btn ">Ver mais</button>
-          </div>
-          <div id="artigos" class="col-lg-4">
-            <img class="blog-img" src="img/jj.jpg" alt="">
-            <h3 class="blog-title">Saciedade: Comer bem e parar de comer na hora certa!</h3>
-            <p class="blog-text">Compreender e respeitar a sensação de saciedade é essencial para comer bem e saber quando dar uma refeição por terminada.</p>
-            <button type="button" class="btn">Ver mais</button>
-          </div>
+
+          <!-- Blog-Text -->
+            <div class="blog-text">
+              <span>Dicas</span>
+              <a href="" class="blog-title">Alimentação Vs Treino</a>
+              <p>Hoje no dia Mundial da Alimentação vamos analisar o impacto da alimentação na atividade física e acredite é bastante!</p>
+              <button type="button" class="btn">Ler Mais</button>
+            </div>
+
         </div>
-      </div>
-      <div class="container-1">
-        <div class="row">
-          <div id="artigos" class="col-lg-4">
-            <img class="blog-img" src="img/pexels-timothy-700446.jpg" alt="">
-            <h3 class="blog-title">Segredos para obter hipertrofia muscular!</h3>
-            <p class="blog-text">Dá-se o nome de hipertrofia muscular ao crescimento da massa muscular alcançado devido ao aumento da secção transversal dos músculos.</p>
-            <button type="button" class="btn ">Ver mais</button>
+
+        <!-- Blog-Box 1 -->
+        <div class="blog-box">
+
+        <!-- Blog-Img -->
+          <div class="blog-img">
+            <img src="img/pexels-leon-ardho-1552242.jpg" alt="">
           </div>
-          <div id="artigos" class="col-lg-4">
-            <img class="blog-img" src="img/kk.jpg" alt="">
-            <h3 class="blog-title">Ser saudável durante a pandemia...</h3>
-            <p class="blog-text">Desde que a pandemia de COVID-19 se instalou no país e no mundo, ser saudável tornou-se ainda mais importante. A saúde e o bem-estar passaram a ser mais valorizados por todos.</p>
-            <button type="button" class="btn ">Ver mais</button>
-          </div>
-          <div id="artigos" class="col-lg-4">
-            <img class="blog-img" src="img/pexels-nathan-cowley-1153372.jpg" alt="">
-            <h3 class="blog-title">Sabe o que comer antes e depois do treino?</h3>
-            <p class="blog-text">Para quem se exercita diariamente, é fundamental saber o que comer antes e depois dos treinos.</p>
-            <button type="button" class="btn ">Ver mais</button>
-          </div>
+
+          <!-- Blog-Text -->
+            <div class="blog-text">
+              <span>Treino</span>
+              <a href="" class="blog-title">Crossfit: Descubra 10 exercícios para um treino completo!</a>
+              <p>O foco do CrossFit é, sobretudo, realizar conjuntos de exercícios num determinado tempo. Desse modo, constitui-se como um desafio de superação individual ou coletiva.</p>
+              <button type="button" class="btn">Ler Mais</button>
+            </div>
+
         </div>
+
+        <!-- Blog-Box 1 -->
+        <div class="blog-box">
+
+        <!-- Blog-Img -->
+          <div class="blog-img">
+            <img src="img/jj.jpg" alt="">
+          </div>
+
+          <!-- Blog-Text -->
+            <div class="blog-text">
+              <span>Dicas</span>
+              <a href="" class="blog-title">Saciedade: Comer bem e parar de comer na hora certa!</a>
+              <p>Compreender e respeitar a sensação de saciedade é essencial para comer bem e saber quando dar uma refeição por terminada.</p>
+              <button type="button" class="btn">Ler Mais</button>
+            </div>
+
+        </div>
+
+        
       </div>
-    </div>
+       <!-- ------------------------------------------------------- -->
+      <!-- Blog-Container -->
+      <div class="blog-container">
+
+      <!-- Blog-Box 1 -->
+        <div class="blog-box">
+
+        <!-- Blog-Img -->
+          <div class="blog-img">
+            <img src="img/pexels-timothy-700446.jpg" alt="">
+          </div>
+
+          <!-- Blog-Text -->
+            <div class="blog-text">
+              <span>Treino</span>
+              <a href="" class="blog-title">Segredos para obter hipertrofia muscular!</a>
+              <p>Dá-se o nome de hipertrofia muscular ao crescimento da massa muscular alcançado devido ao aumento da secção transversal dos músculos.</p>
+              <button type="button" class="btn">Ler Mais</button>
+            </div>
+
+        </div>
+
+        <!-- Blog-Box 1 -->
+        <div class="blog-box">
+
+        <!-- Blog-Img -->
+          <div class="blog-img">
+            <img src="img/kk.jpg" alt="">
+          </div>
+
+          <!-- Blog-Text -->
+            <div class="blog-text">
+              <span>Dicas</span>
+              <a href="" class="blog-title">Ser saudável durante a pandemia...</a>
+              <p>Desde que a pandemia de COVID-19 se instalou no país e no mundo, ser saudável tornou-se ainda mais importante. A saúde e o bem-estar passaram a ser mais valorizados por todos.</p>
+              <button type="button" class="btn">Ler Mais</button>
+            </div>
+
+        </div>
+
+        <!-- Blog-Box 1 -->
+        <div class="blog-box">
+
+        <!-- Blog-Img -->
+          <div class="blog-img">
+            <img src="img/pexels-nathan-cowley-1153372.jpg" alt="">
+          </div>
+
+          <!-- Blog-Text -->
+            <div class="blog-text">
+              <span>Dicas</span>
+              <a href="" class="blog-title">Sabe o que comer antes e depois do treino?</a>
+              <p>Para quem se exercita diariamente, é fundamental saber o que comer antes e depois dos treinos.</p>
+              <button type="button" class="btn">Ler Mais</button>
+            </div>
+
+        </div>
+
+        
+      </div>
+
 
 @endsection
