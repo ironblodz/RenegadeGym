97c37f7e5e25869d0da43cbb9c9dfd5a706be84f
Pedro6Miguel <pedro.miguel.16@hotmail.com>
2021-11-12 21:03:04 +0000

    Update

--- resources/views/layout/partials/navbar.blade.php
+++ resources/views/layout/partials/navbar.blade.php
@@ -3,7 +3,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a href="/"><img class="logo-img" src="img/logo.png" alt=""></a>
+        <a href="/index"><img class="logo-img" src="img/logo.png" alt=""></a>
         <button id="bg-color-toggler" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <i class="fa fa-bars" aria-hidden="true"></i>
         </button>
