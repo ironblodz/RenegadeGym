8d9cc60ee81bf6108d7e57eb0e8efaf5aeed62f5
ironblodz <naylestorm12@gmail.com>
2021-11-17 01:38:11 +0000

    update

--- resources/views/gm/contacts.blade.php
+++ resources/views/gm/contacts.blade.php
@@ -13,7 +13,7 @@
 <div class="container">
     <div class="form">
         <div class="contact-info">
-            <h3 class="title">Esclareça as suas dúvidas connosco</h3>
+            <h3 class="title">Esclareça as suas dúvidas</h3>
             <p class="text"> Preencha o formulário com o que é pedido </p>
             <div class="info">
                 <div class="social-information"> <i class="fa fa-map-marker"></i>
@@ -28,16 +28,16 @@
             </div>
             <div class="social-media">
                 <p>Conecte-se conosco :</p>
-                <div class="social-icons"> <a href="#"> <i class="fa fa-facebook-f"></i> </a> <a href="#"> <i class="fa fa-twitter"></i> </a> <a href="#"> <i class="fa fa-instagram"></i> </a> <a href="#"> <i class="fa fa-linkedin"></i> </a> </div>
+                <div class="social-icons"> <a href="#"> <i class="fa fa-facebook-f"></i> </a> <a href="#"> <i class="fa fa-twitter"></i> </a> <a href="#"> <i class="fa fa-instagram"></i> </a>  </div>
             </div>
         </div>
         <div class="contact-info-form"> <span class="circle one"></span> <span class="circle two"></span>
             <form action="#" onclick="return false;" autocomplete="off">
-                <h3 class="title">Escreva-nos</h3>
-                <div class="social-input-containers"> <input type="text" name="name" class="input" placeholder="Name" /> </div>
+                <h3 class="title">Fale connosco</h3>
+                <div class="social-input-containers"> <input type="text" name="name" class="input" placeholder="Nome" /> </div>
                 <div class="social-input-containers"> <input type="email" name="email" class="input" placeholder="Email" /> </div>
-                <div class="social-input-containers"> <input type="tel" name="phone" class="input" placeholder="Phone" /> </div>
-                <div class="social-input-containers textarea"> <textarea name="message" class="input" placeholder="Message"></textarea> </div> <input type="submit" value="Send" class="btn" />
+                <div class="social-input-containers"> <input type="tel" name="phone" class="input" placeholder="Telefone" /> </div>
+                <div class="social-input-containers textarea"> <textarea name="message" class="input" placeholder="Mensagem"></textarea> </div> <input type="submit" value="Enviar" class="btn" />
             </form>
         </div>
     </div>
