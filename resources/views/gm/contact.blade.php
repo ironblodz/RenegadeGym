bb046e899a916bcfb128ee856ded6ae41c4fd783
ironblodz <naylestorm12@gmail.com>
2021-12-06 22:56:22 +0000

    update

--- resources/views/gm/contact.blade.php
+++ resources/views/gm/contact.blade.php
@@ -36,7 +36,7 @@
 
 
 	<div class="container-contact100">
-		<div class="contact100-map" data-scrollwhell="0" data-draggable="2" ><img src="img/gps1.png" height="1800px" width="2000px" ></div>
+		<div class="contact100-map" data-scrollwhell="0" data-draggable="2" ><img src="img/gps1.png" height="2100px" width="2000px" ></div>
 
 		<button class="contact100-btn-show">
 			<i class="fa fa-envelope-o" aria-hidden="true"></i>
