2f106a96db83cf3810ad0a3a7c80e7a2016a094d
ironblodz <naylestorm12@gmail.com>
2021-12-07 16:40:07 +0000

    update

--- resources/views/gm/Sobrenos.blade.php
+++ resources/views/gm/Sobrenos.blade.php
@@ -23,8 +23,8 @@
     <!-- Hero section -->
     <section id="hero" class="text-white tm-font-big tm-parallax">
       <!-- Navigation -->
-      
-      
+
+
 
       <div class="text-center tm-hero-text-container">
         <div class="tm-hero-text-container-inner">
@@ -53,15 +53,12 @@
             <div class="tm-intro-text-container">
                 <h2 class="tm-text-danger mb-4 tm-section-title">Introdução</h2>
                 <p class="mb-4 tm-intro-text">
-                  The Town is <strong>simple CSS template</strong> built on Bootstrap v4.1.3
-                  and this is a little parallax layout that you can use for your websites.
+                  A instituição Renegade é uma entidade sem fins lucrativos que foi criada com o ambito
+de ajudar e oferecer o bem estar fisico e mental ao utiliziador, oferecendo uma variedade
+de serviços para o ajudar a melhorar o seu estado de vida com o desporto.
               </p>
-                <p class="mb-5 tm-intro-text">
-                  Please tell your friends about our site
-                  Curabitur dapibus tristique enim a imperdiet. Etiam
-              tristique sem sed condimentum posuere. </p>
                 <div class="tm-next">
-                  <a href="#work" class="tm-intro-text btn btn-danger">Read More</a>
+                  <a href="#work" class="tm-intro-text btn btn-danger">Ler mais</a>
                 </div>
             </div>
           </div>
@@ -70,37 +67,31 @@
         <div class="row tm-section-pad-top">
           <div class="col-lg-4">
             <i class="fas fa-4x fa-home text-center tm-icon"></i>
-            <h4 class="text-center text-danger mb-4">Curabitur at elit eu risus</h4>
+            <h4 class="text-center text-danger mb-4">Ginásios</h4>
             <p>
-              Sed ultrices sit amet mi eu malesuada. Cras ultricies gravida
-              nisi, ac pellentesque nunc tincidunt quis. Aenean at ornare lacus.
-              Duis imperdiet lacus justo.
+              Estamos abertos em 5 cidades do paí, façanos uma visita e usufrua do melhor e único ginásio grátis.
             </p>
           </div>
 
         <div class="col-lg-4 mt-5 mt-lg-0">
           <i class="fas fa-4x fa-users text-center tm-icon"></i>
-          <h4 class="text-center text-danger  mb-4">Nunc sed metus vel nisi</h4>
+          <h4 class="text-center text-danger  mb-4">Aulas de grupo</h4>
           <p>
-            Praesent ut finibus leo. Duis et tempus ipsum, id pretium nunc.
-            Vivamus imperdiet sem quis orci pharetra convallis. Nunc a tempus
-            nisi, sed fringilla purus. In hac habitasse platea.
+           Ofereçemos uma basta variedade de aulas de grupo todos os dias, é só fazer a sua reserva e poderá participar em cada uma, todas elas em diferentes horários
           </p>
         </div>
         <div class="col-lg-4 mt-5 mt-lg-0">
           <i class="fas fa-4x fa-tags text-center tm-icon"></i>
-          <h4 class="text-center text-danger mb-4">Fusce sed nisi sagittis</h4>
+          <h4 class="text-center text-danger mb-4">Modalidades</h4>
           <p>
-            Donec vestibulum libero nisl. Curabitur ac orci ac lorem blandit
-            volutpat. Sed ac sodales nibh, ut porttitor elit. Sed id dui mi.
-            Vestibulum ante ipsum primis in faucibus.
+            Escolhemos a dedo as modalidades, todas elas importantes e com diferentes objetivos para o cliente
           </p>
         </div>
       </div>
     </section>
- 
 
-   
+
+
     </section>
     <script src="js/jquery-1.9.1.min.js"></script>
     <script src="slick/slick.min.js"></script>
