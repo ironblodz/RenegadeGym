4e243ba8aa0cba4d18fa4a1ba440cdbeeb5888c1
ironblodz <naylestorm12@gmail.com>
2021-12-07 15:24:37 +0000

    update

--- resources/views/gm/Sobrenos.blade.php
+++ resources/views/gm/Sobrenos.blade.php
@@ -1,59 +1,259 @@
-  @extends('master')
+@extends ('master')
 
-  @section('title', 'Sobre')
-
-  @section('content')
-    <link rel="stylesheet" href="{{asset('css/nicepage.css')}}" media="screen">
-    <link rel="stylesheet" href="{{asset('css/Sobrenos.css')}}" media="screen">
-    <script class="u-script" type="text/javascript" src="{{asset('js/jquery.js')}}" defer=""></script>
-    <script class="u-script" type="text/javascript" src="{{asset('js/nicepage.js')}}" defer=""></script>
-    <meta name="generator" content="Nicepage 3.27.0, nicepage.com">
-    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
-    
-    
-    
-    <script type="application/ld+json">{
-		"@context": "http://schema.org",
-		"@type": "Organization",
-		"name": ""
-}</script>
-    <meta name="theme-color" content="#478ac9">
-    <meta property="og:title" content="Sobrenos">
-    <meta property="og:type" content="website">
+@section ('content')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>The Town HTML CSS Template</title>
+    <link rel="stylesheet" href="fontawesome-5.5/css/all.min.css" />
+    <link rel="stylesheet" href="{{asset('slick/slick.css')}}">
+    <link rel="stylesheet" href="{{asset('slick/slick-theme.css')}}">
+    <link rel="stylesheet" href="{{asset('magnific-popup/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('css/templatemo-style.css')}}" />
+    <!--
+	The Town
+	https://templatemo.com/tm-525-the-town
+	-->
   </head>
-  <body class="u-body"><header class="u-clearfix u-header u-header" id="sec-ea93"><div class="u-clearfix u-sheet u-sheet-1"></div></header>
-    <section class="u-clearfix u-section-1" id="sec-12f4">
-      <div class="u-clearfix u-sheet u-valign-middle-xs u-sheet-1">
-        <div class="u-clearfix u-expanded-width u-gutter-100 u-layout-wrap u-layout-wrap-1">
-          <div class="u-layout">
-            <div class="u-layout-row">
-              <div class="u-align-left u-container-style u-layout-cell u-size-30 u-layout-cell-1">
-                <div class="u-container-layout u-valign-bottom-lg u-valign-bottom-md u-valign-bottom-sm u-valign-bottom-xl u-container-layout-1">
-                  <img class="u-image u-image-default u-image-1" src="img/Untitled-111.png" alt="" data-image-width="500" data-image-height="500">
+  <body>
+    <!-- Hero section -->
+    <section id="hero" class="text-white tm-font-big tm-parallax">
+      <!-- Navigation -->
+      
+      
+
+      <div class="text-center tm-hero-text-container">
+        <div class="tm-hero-text-container-inner">
+            <h2 class="tm-hero-title">Quem nós somos?</h2>
+            <p class="tm-hero-subtitle">
+              Somos uma instituição sem fins lucrativos
+              <br> Mais informações, abaixo..
+            </p>
+        </div>
+      </div>
+
+      <div class="tm-next tm-intro-next">
+        <a href="#introduction" class="text-center tm-down-arrow-link">
+          <i class="fas fa-3x fa-caret-down tm-down-arrow"></i>
+        </a>
+      </div>
+    </section>
+
+    <section id="introduction" class="tm-section-pad-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <img src="img/logo.png" alt="Image" class="img-fluid tm-intro-img" />
+          </div>
+          <div class="col-lg-6">
+            <div class="tm-intro-text-container">
+                <h2 class="tm-text-danger mb-4 tm-section-title">Introdução</h2>
+                <p class="mb-4 tm-intro-text">
+                  The Town is <strong>simple CSS template</strong> built on Bootstrap v4.1.3
+                  and this is a little parallax layout that you can use for your websites.
+              </p>
+                <p class="mb-5 tm-intro-text">
+                  Please tell your friends about our site
+                  Curabitur dapibus tristique enim a imperdiet. Etiam
+              tristique sem sed condimentum posuere. </p>
+                <div class="tm-next">
+                  <a href="#work" class="tm-intro-text btn btn-danger">Read More</a>
                 </div>
-              </div>
-              <div class="u-align-center u-container-style u-layout-cell u-size-30 u-layout-cell-2">
-                <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-valign-middle-xl u-container-layout-2">
-                  <h2 class="u-text u-text-default-lg u-text-default-md u-text-default-sm u-text-default-xl u-text-1">Sobre nós</h2>
-                  <p class="u-text u-text-2">A instituição Renegade é uma entidade sem fins lucrativos que foi criada com o ambito
-                  de ajudar e oferecer o bem estar fisico e mental ao utiliziador, oferecendo uma variedade
-                  de serviços para o ajudar a melhorar o seu estado de vida com o desporto. </p>
-                </div>
-              </div>
             </div>
           </div>
         </div>
+
+        <div class="row tm-section-pad-top">
+          <div class="col-lg-4">
+            <i class="fas fa-4x fa-home text-center tm-icon"></i>
+            <h4 class="text-center text-danger mb-4">Curabitur at elit eu risus</h4>
+            <p>
+              Sed ultrices sit amet mi eu malesuada. Cras ultricies gravida
+              nisi, ac pellentesque nunc tincidunt quis. Aenean at ornare lacus.
+              Duis imperdiet lacus justo.
+            </p>
+          </div>
+
+        <div class="col-lg-4 mt-5 mt-lg-0">
+          <i class="fas fa-4x fa-users text-center tm-icon"></i>
+          <h4 class="text-center text-danger  mb-4">Nunc sed metus vel nisi</h4>
+          <p>
+            Praesent ut finibus leo. Duis et tempus ipsum, id pretium nunc.
+            Vivamus imperdiet sem quis orci pharetra convallis. Nunc a tempus
+            nisi, sed fringilla purus. In hac habitasse platea.
+          </p>
+        </div>
+        <div class="col-lg-4 mt-5 mt-lg-0">
+          <i class="fas fa-4x -bottom-0fa-tags text-center tm-icon"></i>
+          <h4 class="text-center text-danger mb-4">Fusce sed nisi sagittis</h4>
+          <p>
+            Donec vestibulum libero nisl. Curabitur ac orci ac lorem blandit
+            volutpat. Sed ac sodales nibh, ut porttitor elit. Sed id dui mi.
+            Vestibulum ante ipsum primis in faucibus.
+          </p>
+        </div>
       </div>
     </section>
-    <section class="u-align-center u-clearfix u-image u-shading u-section-2" src="" data-image-width="605" data-image-height="285" id="sec-51c4">
-      <div class="u-clearfix u-sheet u-sheet-1">
-        <h1 class="u-text u-text-default u-title u-text-1">Renegade Gym</h1>
-        <p class="u-large-text u-text u-text-variant u-text-2">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
-        <a href="#" class="u-btn u-button-style u-palette-2-base u-btn-1">Read More</a>
-      </div>
-    </section>
-    
-    
+ 
+
    
+    </section>
+    <script src="js/jquery-1.9.1.min.js"></script>
+    <script src="slick/slick.min.js"></script>
+    <script src="magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.singlePageNav.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script>
+
+      function getOffSet(){
+        var _offset = 450;
+        var windowHeight = window.innerHeight;
+
+        if(windowHeight > 500) {
+          _offset = 400;
+        }
+        if(windowHeight > 680) {
+          _offset = 300
+        }
+        if(windowHeight > 830) {
+          _offset = 210;
+        }
+
+        return _offset;
+      }
+
+      function setParallaxPosition($doc, multiplier, $object){
+        var offset = getOffSet();
+        var from_top = $doc.scrollTop(),
+          bg_css = 'center ' +(multiplier * from_top - offset) + 'px';
+        $object.css({"background-position" : bg_css });
+      }
+
+      // Parallax function
+      // Adapted based on https://codepen.io/roborich/pen/wpAsm
+      var background_image_parallax = function($object, multiplier, forceSet){
+        multiplier = typeof multiplier !== 'undefined' ? multiplier : 0.5;
+        multiplier = 1 - multiplier;
+        var $doc = $(document);
+        // $object.css({"background-attatchment" : "fixed"});
+
+        if(forceSet) {
+          setParallaxPosition($doc, multiplier, $object);
+        } else {
+          $(window).scroll(function(){
+            setParallaxPosition($doc, multiplier, $object);
+          });
+        }
+      };
+
+      var background_image_parallax_2 = function($object, multiplier){
+        multiplier = typeof multiplier !== 'undefined' ? multiplier : 0.5;
+        multiplier = 1 - multiplier;
+        var $doc = $(document);
+        $object.css({"background-attachment" : "fixed"});
+        $(window).scroll(function(){
+          var firstTop = $object.offset().top,
+              pos = $(window).scrollTop(),
+              yPos = Math.round((multiplier * (firstTop - pos)) - 186);
+
+          var bg_css = 'center ' + yPos + 'px';
+
+          $object.css({"background-position" : bg_css });
+        });
+      };
+
+      $(function(){
+        // Hero Section - Background Parallax
+        background_image_parallax($(".tm-parallax"), 0.30, false);
+        background_image_parallax_2($("#contact"), 0.80);
+
+        // Handle window resize
+        window.addEventListener('resize', function(){
+          background_image_parallax($(".tm-parallax"), 0.30, true);
+        }, true);
+
+        // Detect window scroll and update navbar
+        $(window).scroll(function(e){
+          if($(document).scrollTop() > 120) {
+            $('.tm-navbar').addClass("scroll");
+          } else {
+            $('.tm-navbar').removeClass("scroll");
+          }
+        });
+
+        // Close mobile menu after click
+        $('#tmNav a').on('click', function(){
+          $('.navbar-collapse').removeClass('show');
+        })
+
+        // Scroll to corresponding section with animation
+        $('#tmNav').singlePageNav();
+
+        // Add smooth scrolling to all links
+        // https://www.w3schools.com/howto/howto_css_smooth_scroll.asp
+        $("a").on('click', function(event) {
+          if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+              scrollTop: $(hash).offset().top
+            }, 400, function(){
+              window.location.hash = hash;
+            });
+          } // End if
+        });
+
+        // Pop up
+        $('.tm-gallery').magnificPopup({
+          delegate: 'a',
+          type: 'image',
+          gallery: { enabled: true }
+        });
+
+        // Gallery
+        $('.tm-gallery').slick({
+          dots: true,
+          infinite: false,
+          slidesToShow: 5,
+          slidesToScroll: 2,
+          responsive: [
+          {
+            breakpoint: 1199,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }{{asset('
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+        });
+      });
+    </script>
   </body>
+</html>
 @endsection
