5081e91d9fa4707b9b36afa84665fb67c81e15fa
Pedro Miguel Carreira Guerra <2201245@my.ipleiria.pt>
2021-12-09 10:12:35 +0000

    Update

--- resources/views/blogs/partials/add-edit.blade.php
+++ resources/views/blogs/partials/add-edit.blade.php
@@ -1,45 +1,16 @@
 <div class="form-group">
     <label for="inputTitle">Titulo</label>
-    <input type="text" class="form-control @error('title') is-invalid @enderror" name="title"
-    value={{old('title',$blog->title)}} required autocomplete="date" autofocus>
-@error('title')
-    <span class="invalid-feedback" role="alert">
-        <strong>{{ $message }}</strong>
-    </span>
-@enderror
+    <input type="text" class="form-control" name="title" id="inputTitle" value="{{old('title',$blog->title)}}" />
 </div>
 
 <div class="form-group">
     <label for="inputDescription">Descrição</label>
-    <textarea class="form-control @error('content') is-invalid @enderror" name="content"
-    value={{old('content',$blog->content)}} required autocomplete="date" autofocus>
-@error('content')
-    <span class="invalid-feedback" role="alert">
-        <strong>{{ $message }}</strong>
-    </span>
-@enderror
-</div>
-
-<div class="form-group">
-    <label for="datetime">Data</label>
-    <input type="date" class="form-control @error('date') is-invalid @enderror" name="date"
-    value={{old('date',$blog->date)}} required autocomplete="date" autofocus>
-@error('date')
-    <span class="invalid-feedback " role="alert">
-        <strong>{{ $message }}</strong>
-    </span>
-@enderror
+    <textarea class="form-control" name="content" id="inputDescription">{{old('description',$blog->description)}}</textarea>
 </div>
 
 <div class="form-group">
     <label for="inputImage">Imagem</label>
-    <input type="file" class="form-control-file @error('image') is-invalid @enderror" name="image"
-    value={{old('image',$blog->image)}} required autocomplete="date" autofocus>
-@error('image')
-    <span class="invalid-feedback" role="alert">
-        <strong>{{ $message }}</strong>
-    </span>
-@enderror
+    <input type="file" class="form-control-file" name="image" id="inputImage" aria-describedby="fileHelp" />
     <small id="fileHelp" class="form-text text-muted"> Faça upload de um arquivo de imagem válido. O tamanho da imagem não deve ser
         mais de 2 MB </small>
 </div>
@@ -47,11 +18,9 @@
 <div class="form-group">
     <label for="inputCategory">Categoria</label>
     <select name="category" id="inputCategory" class="form-control">
-
        @foreach ($categories as $category)
  <option {{ old('$category', $blog->category_id) == $category->id ? "selected":""}}
  value="{{$category->id}}">{{$category->name}}</option>
  @endforeach
-
     </select>
 </div>
