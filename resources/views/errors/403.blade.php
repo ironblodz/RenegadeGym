107a35bf323c4cdc87e84de3abb10aa04d57e606
ironblodz <naylestorm12@gmail.com>
2021-12-20 13:13:39 +0000

    update

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -5,9 +5,9 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	
 
-	<title>404 HTML Template by Colorlib</title>
+	<title>Error 404</title>
 
 	<!-- Google font -->
 	<link href="https://fonts.googleapis.com/css?family=Muli:400" rel="stylesheet">
@@ -27,7 +27,7 @@
 		<div class="notfound-bg"></div>
 		<div class="notfound">
 			<div class="notfound-404">
-				<h1>404</h1>
+				<h1>Erro 403</h1>
 			</div>
 			<h2>Página não foi encontrada</h2>
 
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,45 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="{{asset('css/503.css')}}">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+</head>
+
+<body>
+    
+    <div class="error-content">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-12 ">
+                    <div class="error-text">
+                        <h1 class="error">Erro 503</h1>
+                        <div class="im-sheep">
+                            <div class="top">
+                                <div class="body"></div>
+                                <div class="head">
+                                    <div class="im-eye one"></div>
+                                    <div class="im-eye two"></div>
+                                    <div class="im-ear one"></div>
+                                    <div class="im-ear two"></div>
+                                </div>
+                            </div>
+                            <div class="im-legs">
+                                <div class="im-leg"></div>
+                                <div class="im-leg"></div>
+                                <div class="im-leg"></div>
+                                <div class="im-leg"></div>
+                            </div>
+                        </div>
+                        <h4>Site em manutenção!</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+    </html>
+
